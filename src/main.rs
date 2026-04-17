49e663cf8367f6fd9d083f4b635e5b2bbe124a86
rlpratyoosh <pratyoosh.prakash.dev@gmail.com>
2026-04-17 21:16:51 +0530

    feat: build simple tcp echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+};
+
+use tokio::{
+    net::{ TcpListener },
+    io::{ BufReader, AsyncBufReadExt, AsyncWriteExt },
+};
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut port = &"8080".to_string();
+    if args.len() > 1 {
+        port = &args[1];
+    };
+    let url = format!("127.0.0.1:{port}");
+
+    let listener = match TcpListener::bind(url).await {
+        Ok(listener) =>{
+            println!("Server running at localhost:{port}");
+            listener
+        },
+        Err(e) => {
+            eprintln!("Error occured while trying to bind socket: {e}");
+            return;
+        }
+    };
+
+    loop {
+        let (mut socket, _) = match listener.accept().await {
+            Ok((socket, addr)) => {
+                println!("Client connected: {:?}", addr);
+                (socket, addr)
+            },
+            Err(e) => {
+                eprintln!("Connection error: {e}");
+                continue;
+            }
+        };
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+            let buf_reader = BufReader::new(reader);
+
+            let mut lines = buf_reader.lines();
+            while let Ok(Some(line)) = lines.next_line().await {
+                if let Err(e) = writer.write_all(line.as_bytes()).await {
+                    eprintln!("Error while writing back to client: {e}");
+                    break;
+                }
+            }
+
+            println!("Client disconnected!");
+        });
+    }
 }

2ee3f044f7a07c79208400263a9645b1d8fe31d8
rlpratyoosh <pratyoosh.prakash.dev@gmail.com>
2026-04-18 10:45:54 +0530

    docs: commentify lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+//! # Fetis library
+//!
+//! `fetis` library provides the core functionality for the Fetis key-value store server. It
+//! includes parsing of client requests and executing commands on the in-memory storage.
+
 use std::{
     sync::{Arc, RwLock},
     collections::HashMap,
 };
 
+// Define query enum for proper query parsing and access
+#[derive(Debug, PartialEq)]
 pub enum Query {
     Ping,
     Set { key: String, value: String },
@@ -10,6 +17,16 @@ pub enum Query {
     Del(String),
 }
 
+/// Parses the given request.
+///
+/// # Examples
+///
+/// ```
+/// let request = vec!["SET", "key", "value"];
+/// let query = fetis::parse(request).unwrap();
+///
+/// assert_eq!(query, fetis::Query::Set { key: "key".to_string(), value: "value".to_string() });
+/// ```
 pub fn parse(request: Vec<&str>) -> Result<Query, String> {
     let response = match request[0].to_uppercase().as_str() {
         "PING" => Ok(Query::Ping),
@@ -40,6 +57,20 @@ pub fn parse(request: Vec<&str>) -> Result<Query, String> {
     response
 }
 
+/// Executes the given query and returns the response as a string.
+///
+/// # Examples
+///
+/// ```
+/// use std::{
+///    sync::{Arc, RwLock},
+///    collections::HashMap,
+/// };
+///
+/// let query = fetis::Query::Ping;
+/// let response = fetis::execute(query, &Arc::new(RwLock::new(HashMap::new())));
+/// assert_eq!(response, "PONG\r\n".to_string());
+/// ```
 pub fn execute(query: Query, storage: &Arc<RwLock<HashMap<String, String>>>) -> String {
     match query {
         Query::Ping => "PONG\r\n".to_string(),
@@ -49,11 +80,14 @@ pub fn execute(query: Query, storage: &Arc<RwLock<HashMap<String, String>>>) -> 
     }
 }
 
+
+// Function to execute a set query
+// Adds given key value pair to the storage
 fn set(key: &str, value: &str, storage: &Arc<RwLock<HashMap<String, String>>>) -> String {
     let mut map = match storage.write() {
         Ok(map) => map,
         Err(e) => {
-            eprintln!("Error occured while aquiring write lock: {e}");
+            eprintln!("Error occured while acquiring write lock: {e}");
             return "ERROR: Internal Server Error\r\n".to_string();
         }
     };
@@ -64,11 +98,14 @@ fn set(key: &str, value: &str, storage: &Arc<RwLock<HashMap<String, String>>>) -
     "Done\r\n".to_string()
 }
 
+// Function to execute a get query
+// Returns the value of the given key if it exists, otherwise returns "Not found\r\n"
+// Returns everything in the storage if the key is "all"
 fn get(key: &str, storage: &Arc<RwLock<HashMap<String,String>>>) -> String {
     let map = match storage.read() {
         Ok(map) => map,
         Err(e) => {
-            eprintln!("Error occured while aquiring read lock: {e}");
+            eprintln!("Error occured while acquiring read lock: {e}");
             return "ERROR: Internal Server Error\r\n".to_string();
         }
     };
@@ -83,11 +120,13 @@ fn get(key: &str, storage: &Arc<RwLock<HashMap<String,String>>>) -> String {
     }
 }
 
+// Function to execute a delete query
+// Deletes the given key from the storage if it exists, otherwise returns "Not found\r\n"
 fn delete(key: &str, storage: &Arc<RwLock<HashMap<String,String>>>) -> String {
     let mut map = match storage.write() {
         Ok(map) => map,
         Err(e) => {
-            eprintln!("Error occured while aquiring write lock: {e}");
+            eprintln!("Error occured while acquiring write lock: {e}");
             return "ERROR: Internal Server Error\r\n".to_string();
         }
     };
